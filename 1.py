f1cbbd99082ddf888776e716b8df33ad263cddc9
Nastya Chichagova <aechichagova@gmail.com>
2021-09-30 18:24:29 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+A='Nastya'
+B='Chihagova'
+C=2002
+print (A, B, C, sep='_')
+A, B = B, A
+C+=60
+print (A, B, C, sep='_')
---
+++ 3.py
@@ -0,0 +1,6 @@
+r=float(input('Введите радиус круга: '))
+pi=3.14
+if (r>0):
+    print ('S = ', pi*(r**2))
+else:
+    print('Ошибка!')
